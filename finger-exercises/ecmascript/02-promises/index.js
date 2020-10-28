2e49e11571cbf2bf02a027745752744d671e9c22
jvente <jvente18@gmail.com>
2020-10-28 16:22:24 -0500

    promises test

--- finger-exercises/ecmascript/02-promises/index.js
+++ finger-exercises/ecmascript/02-promises/index.js
@@ -1,9 +1,9 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+    return new Promise(resolve => setInterval(resolve(time), Date.now()));
 }
 
-export function asyncDelay() {
-
+export function asyncDelay(time) {
+    return new Promise(resolve => resolve(time));
 }
