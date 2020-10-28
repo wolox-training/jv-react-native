a91d71c46deeed381d270ad53c03338d52328fbe
jvente <jvente18@gmail.com>
2020-10-28 16:26:05 -0500

    spread operator test

--- finger-exercises/ecmascript/01-spread/index.js
+++ finger-exercises/ecmascript/01-spread/index.js
@@ -2,9 +2,44 @@ import { isArray } from './utils';
 
 export function min() {
   isArray();
-  return Math.min();
+  if (arguments === undefined) return undefined;
+
+  let myParams = [...arguments];
+
+  if (typeof myParams[0] === "object") {
+    let myNew = myParams[0];
+    return myNew.sort()[0];
+  }
+
+  return myParams.sort()[0];
 }
 
 export function copy() {
+  let type = arguments['0'].constructor.name;
+  if (type === "Object") return { ...arguments['0'] }
+  return [...arguments['0']];
 
+}
+export function reverseMerge() {
+  let newArray = [...arguments];
+  let data = newArray[0];
+  let newData;
+  for (let i in newArray) {
+    if (i !== 0) {
+      newData = [...newArray[i], ...data];
+    }
+  }
+  return newData;
+}
+
+export function filterAttribs() {
+
+  let newObject = {};
+  Object.entries(...arguments).map((key) => {
+    if (key[0] !== "a" && key[0] !== "b") {
+      newObject[key[0]] = key[1];
+    }
+  });
+
+  return newObject;
 }
--- finger-exercises/ecmascript/01-spread/utils.js
+++ finger-exercises/ecmascript/01-spread/utils.js
@@ -2,8 +2,8 @@
 // export default
 // Would you like to fix this issue ?
 
-export function isArray(aPossibleArray) {
-  return aPossibleArray.constructor === Array;
+export function isArray(aPossibleArray = []) {
+  return aPossibleArray.constructor.name === "Array";
 }
 
-// export default isArray;
+export default isArray;
