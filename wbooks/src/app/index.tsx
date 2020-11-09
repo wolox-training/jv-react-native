37625ab13b3466e84c6de78b5c80e8718d78e4f7
jvente <jvente18@gmail.com>
2020-11-09 14:57:44 -0500

    structure library better and destructuring on FlatList in its prop keyExtractor

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
 import Library from '@screens/Library';
 
-import styles from './styles';
-
 function App() {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.body}>
-        <Library />
-      </SafeAreaView>
-    </>
-  );
+  return <Library />;
 }
 
 export default App;
--- wbooks/src/app/screens/Library/index.tsx
+++ wbooks/src/app/screens/Library/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { FlatList, ListRenderItem } from 'react-native';
+import { FlatList, ListRenderItem, SafeAreaView, StatusBar } from 'react-native';
 import { BOOKS_MOCK } from '@constants/mockBooks';
 import { Book as IBook } from '@interfaces/book';
 
 import Book from './components/Book';
+import styles from './styles';
 
 function Library() {
   const renderBook: ListRenderItem<IBook> = ({ item }) => <Book book={item} />;
 
-  return <FlatList data={BOOKS_MOCK} renderItem={renderBook} keyExtractor={item => `${item.id}`} />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.body}>
+        <FlatList data={BOOKS_MOCK} renderItem={renderBook} keyExtractor={({ id }) => `${id}`} />
+      </SafeAreaView>
+    </>
+  );
 }
 
 export default Library;
