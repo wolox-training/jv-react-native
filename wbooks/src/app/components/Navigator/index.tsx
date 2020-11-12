0d840d905427eabc5f19a391913a2ef5ca36ba5e
jvente <jvente18@gmail.com>
2020-11-12 09:22:02 -0500

    add routes to navigator from constants

--- wbooks/src/app/components/Navigator/index.tsx
+++ wbooks/src/app/components/Navigator/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Routes from '@constants/routes';
 import Library from '@screens/Library';
 import DetailBook from '@screens/DetailBook';
 
@@ -10,8 +11,8 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Library} options={{ title: 'Library' }} />
-        <Stack.Screen name="Detail" component={DetailBook} />
+        <Stack.Screen name={Routes.Home} component={Library} options={{ title: 'Library' }} />
+        <Stack.Screen name={Routes.Detail} component={DetailBook} />
       </Stack.Navigator>
     </NavigationContainer>
   );
